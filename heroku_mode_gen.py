37165ba63d80b6a12161df0581909641bff4684f
Jonathan Metzman <jon.metzman@gmail.com>
2013-10-30 20:17:42 -0400

    Fixed bug where newline wasn't being added between commands and additional commands, which messed up parsing. Commands are now assigned sane keybindings

--- heroku_mode_gen.py
+++ heroku_mode_gen.py
@@ -24,7 +24,7 @@ def key_bindings(commands):
     open_keys = [key for key in keys if key not in taken and key.isalpha()] # Less special chars
     key_binding_funcs = []
     for command in commands:
-        key = pick_key_binding_letter(cmd, open_keys)
+        key = pick_key_binding_letter(command, open_keys)
         open_keys.remove(key)
         key_binding_funcs.append(gen_key_binding_func(command, key))
     return key_binding_funcs
@@ -33,26 +33,32 @@ def key_bindings(commands):
 def gen_key_binding_func(cmd, key):
     return '(global-set-key (kbd "C-c %s") \'%s)\n' % (key, cmd)
 
+def is_in(letter, string):
+    if letter in string:
+        return letter
+    elif letter.upper() in string:
+        return letter.upper()
+    else:
+        return ''
+
 def pick_key_binding_letter(cmd, open_keys):
-    if cmd[0] in open_keys:
-        return cmd[0]
-    elif cmd[0].upper in open_keys:
-        return cmd.upper
-    elif open_keys == '':
+    if open_keys == '':
         print "NOT ENOUGH KEYS"
         raise Exception
-    else:
-        reverse_keys = open_keys[::-1] # choose uppercase only if easy to remember
-        return reverse_keys[0]
+    for letter in cmd:
+        if is_in(letter, open_keys):
+            return is_in(letter, open_keys)
+    reverse_keys = open_keys[::-1] # choose uppercase only if easy to remember
+    return reverse_keys[0]
 
 def create_elisp_cmd(commmented_cmd):
-    return '(defun heroku-mode-%s()\n\t"%s"\n\t(interactive)\n\t(shell-command "%s")\n)\n\n' % (commmented_cmd[0], commmented_cmd[1], commmented_cmd[0])
+    return '(defun heroku-mode-%s()\n\t"%s"\n\t(interactive)\n\t(shell-command "heroku %s")\n)\n\n' % (commmented_cmd[0], commmented_cmd[1], commmented_cmd[0])
     
 
 if __name__=='__main__':
     text = open("help.txt").read()
     commands_text_list = parse_list(text, ":\n\n", "\n\n")
-    commands_text = "\n"+commands_text_list[0] + commands_text_list[1]
+    commands_text = "\n"+commands_text_list[0] + '\n' + commands_text_list[1]
     command_docs = parse_list(commands_text, "#  ", "\n")
     command_docs = [cmd_doc.strip(' ') for cmd_doc in command_docs]
     commands = parse_list(commands_text,"\n  ", "#  ")
@@ -60,4 +66,4 @@ if __name__=='__main__':
     commented_cmds = zip(commands, command_docs)
     elisp = ''.join([create_elisp_cmd(commented_cmd) for commented_cmd in commented_cmds])
     elisp += ''.join(key_bindings(commands))
-
+    print elisp
