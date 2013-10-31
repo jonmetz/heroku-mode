98ffd8331996d2643fb9c13d0d56409d1451a3b0
Jonathan Metzman <jon.metzman@gmail.com>
2013-10-30 22:47:10 -0400

    Added heroku-mode-app which user can set to specify an app to the heroku cli. Added heroku-mode-execute which will take into account whether heroku-mode-app has been specified before executing a bash command. This function will be used by all functions (except heroku-mode-push) instead of shell-command.

--- heroku-mode.el
+++ heroku-mode.el
@@ -6,19 +6,30 @@
   "A mode to improve workflow with the heroku command line tool"
   :version "0.0.1")
 
+;; Set app to "" if not specified
+(setq heroku-mode-app "")
+
 ;; Heroku tool commands
 
 (defun heroku-mode-open()
   "Opens default app homepage on default web browser"
   (interactive)
-  (shell-command "heroku open")
-)
+  (shell-command "heroku open" ))
 
 (defun heroku-mode-push()
   "Deploys app to Heroku"
   (interactive)
-  (shell-command "git push heroku master")
-)
+  (shell-command "git push heroku master"))
+
+(defun heroku-mode-execute (cmd)
+	 (if (not (string-equal heroku-mode-app ""))
+	     (shell-command (format "%s --app %s" cmd heroku-mode-app))
+	   (shell-command cmd)))
+
+(defun heroku-mode-set-app(app-name)
+  "Set the name of the app with which all other functions will be called"
+  (interactive "sEnter app's name: ")
+  (setq heroku-mode-app (format "%s" app-name)))
 
 ;; Keybindings for the above Heroku commands in elisp
 ;; Did not start with most intuitive prefix C-h since this is reserved for help in emacs
